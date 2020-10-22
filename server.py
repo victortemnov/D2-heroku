faf8716ecb2245d0486acd2ab146158e31562bce
ekke <63651605+victortemnov@users.noreply.github.com>
2020-10-22 19:54:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,36 +1,34 @@
-import os
 import sentry_sdk
-from bottle import Bottle, HTTPResponse
+import os
+from bottle import run, route, HTTPResponse
 from sentry_sdk.integrations.bottle import BottleIntegration
 
 SENTRY_DSN = "https://d38b13cc54fe49a3869b0aa349af4184@o464474.ingest.sentry.io/5473342"
 
-sentry_sdk.init(dsn=SENTRY_DSN, integrations=[BottleIntegration()])
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[BottleIntegration()]
+)
 
-app = Bottle()
-
-@app.route("/")
+@route("/")
 def index():
-    return HTTPResponse(status=200, body="append to ulr <b>/success</b> or <b>/fail</b>")
+    return HTTPResponse(status=200, body="append to url /success or /fail")
 
-@app.route("/success")
-def success():
-    return HTTPResponse(status=200, body="Success")
+@route("/success")
+def sucess():
+    return HTTPResponse(status=200, body="OK")
 
-@app.route("/fail")
+@route("/fail")
 def fail():
-    raise RuntimeError("Server error!")
+    raise RuntimeError("Server error")
 
-if os.environ.get('SERVER_URL') == 'https://heroku-d2-hw.herokuapp.com':
-    app.run(
+
+if __name__ == "__main__":
+
+    run(
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 5000)),
         server='gunicorn',
         workers=3
     )
-else:
-    app.run(
-        host='localhost',
-        port=8080,
-        debug=True
-    )
+    
